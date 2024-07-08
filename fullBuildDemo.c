c43640e06306d027742144889fb8016b7eb4f5db
sponge216 <yoavatia123@gmail.com>
2024-07-08 16:35:25 +0300

    Started integrating notifications

--- fullBuildDemo.c
+++ fullBuildDemo.c
@@ -6,10 +6,13 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include "structsAndGlobals.h"
+notif_line_ptr getNewNotifsLinePtr();
+#include "setFunctions.h"
 
 int createGeneralMenu(char *[], int, int *, int *);
 int exitFunction(int *, int *);
 int notificationsMenu(int *, int *);
+void notificationsWindow(void *);
 int filterMenu(int *offset, int *lineCount);
 int refreshRateMenu(int *offset, int *lineCount);
 int sortMenu(int *offset, int *lineCount);
@@ -22,10 +25,15 @@ WINDOW *createSubWindow(WINDOW *, int, int, int, int);
 ps_line_ptr getNewPsLinePtr();
 int freePsLinePtr(ps_line_ptr);
 int printLines(ps_line_ptr, WINDOW *, int, int, int);
-int freeAllLines(ps_line_ptr);
+int freeAllPsLines(ps_line_ptr);
 int getLinesForProc(ps_line_ptr, int *);
 int addLinesToProc(WINDOW *, int, int *, int *);
 void createProcWindow(void *);
+
+int printNotifLines(WINDOW *, notif_line_ptr, int, int, int);
+notif_line_ptr getNewNotifsLinePtr();
+int getSetsForNotifs(hash_set_ptr, hash_set_ptr, hash_set_ptr, int *);
+int addLinesToNotifications(WINDOW *, hash_set_ptr, notif_line_ptr, int, int *, int *);
 
 char outputCommand[MAX_COMMAND_SIZE] = DEFAULT_COMMAND;
 int waitOnInput; // Used to time mainWindow and procWindow's inputs so they dont collide.
@@ -44,7 +52,227 @@ int exitFunction(int *offset, int *lineCount)
 {
     return 0;
 }
+int printNotifLines(WINDOW *win, notif_line_ptr line, int offset, int scrnWidth, int scrnHeight)
+{
+    if (line == NULL)
+        return 1;
 
+    int currLine = 0;
+    int lastLine = LINES_ON_SCREEN;
+
+    while (currLine < offset)
+    {
+        line = line->next;
+        currLine++;
+    }
+    lastLine += currLine;
+    int y = INIT_PRINT_Y;
+    lastLine -= y;
+    while (line->next && currLine < lastLine)
+    {
+        notif_line_ptr t = line;
+        for (int i = 0; i < NOTIF_ARR_SIZE; i++)
+        {
+            attron(COLOR_PAIR(1));
+            mvwprintw(win, y, i * (scrnWidth / NOTIF_ARR_SIZE) + 2, "%s", line->info_arr[i]);
+            wrefresh(win);
+            attroff(COLOR_PAIR(1));
+        }
+        y++;
+        line = line->next;
+        currLine++;
+    }
+    wrefresh(win);
+    return 0;
+}
+notif_line_ptr getNewNotifsLinePtr()
+{
+    notif_line_ptr line = (notif_line_ptr)malloc(sizeof(notif_line));
+    if (line == NULL)
+        return NULL;
+
+    for (int i = 0; i < NOTIF_ARR_SIZE; i++)
+    {
+        line->info_arr[i] = NULL;
+    }
+    line->next = NULL;
+    return line;
+}
+
+int getSetsForNotifs(hash_set_ptr genSet, hash_set_ptr newProcsSet, hash_set_ptr oldProcsSet, int *lineCount)
+{
+
+    FILE *fp = popen(DEFAULT_NOTIF_COMMAND, "r");
+    if (fp == NULL)
+    {
+        return 1;
+    }
+
+    hash_set_ptr newSet = getNewHashSet();
+    notif_line_ptr line = getNewNotifsLinePtr();
+
+    char fileBuffer[BUFF_SIZE];
+    char lineBuffer[BUFF_SIZE];
+    char *procInfoArr[2];
+    int procInfoArrSize = 2;
+    *lineCount = 0;
+    int currInfoIndex = 0;
+    int lineBuffIndex = 0;
+    int len = 0;
+
+    while (fgets(fileBuffer, sizeof(fileBuffer), fp) != NULL)
+    {
+        int i = 0;
+        while (i < BUFF_SIZE)
+        {
+            int startIndex = i;
+            while (i < BUFF_SIZE && fileBuffer[i] != ' ' && fileBuffer[i] != '\n')
+            {
+                lineBuffer[lineBuffIndex] = fileBuffer[i];
+                i++;
+                lineBuffIndex++;
+            }
+            len += i - startIndex;
+
+            if (i >= BUFF_SIZE)
+            { // stopped because of end of read.
+                break;
+            }
+            // dump the line's data into a line struct.
+            // TODO: check errors malloc
+            procInfoArr[currInfoIndex] = (char *)malloc(sizeof(char) * (len + 1));
+            strncpy(procInfoArr[currInfoIndex], lineBuffer, len);
+            procInfoArr[currInfoIndex][len] = '\0';
+
+            len = 0;
+            lineBuffIndex = 0;
+
+            if (currInfoIndex == procInfoArrSize - 1) // check if it's the end of the line.
+            {
+                addProcToSetByName(newSet, procInfoArr[1], strtoll(procInfoArr[0], NULL, 10)); // Adding the current process' pid and name to newSet.
+                (*lineCount)++;
+                currInfoIndex = 0;
+                for (int i = 0; i < procInfoArrSize; i++) // freeing line's allocated variables.
+                {
+                    free(procInfoArr[i]);
+                }
+
+                break;
+                // TODO: fix it so if a line is bigger than BUFF_SIZE it wont break.
+            }
+            else
+                currInfoIndex++;
+            i++;
+        }
+    }
+    newProcsSet = compareSets(newSet, genSet);
+    oldProcsSet = compareSets(genSet, newSet);
+
+    freeHashSet(newSet);
+
+    pclose(fp);
+    return 0;
+}
+int addLinesToNotifications(WINDOW *win, hash_set_ptr genSet, notif_line_ptr lines, int height, int *offset, int *lineCount)
+{
+
+    // do lines
+    werase(win);
+    box(win, 0, 0);
+
+    hash_set_ptr newProcsSet;
+    hash_set_ptr oldProcsSet;
+
+    int res = getSetsForNotifs(genSet, newProcsSet, oldProcsSet, lineCount); // Get lines to print.
+    if (res)                                                                 // check for errors in getting the lines.
+    {
+        wrefresh(win);
+        return 1;
+    }
+
+    removeSetFromSet(genSet, oldProcsSet);
+    addSetToSet(genSet, newProcsSet);
+
+    notif_line_ptr temp = lines;
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = turnSetIntoNotifLines(oldProcsSet, "STOPPED");
+
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = turnSetIntoNotifLines(newProcsSet, "STARTED");
+
+    int scrnHeight, scrnWidth;
+    getmaxyx(mainWindow, scrnHeight, scrnWidth);
+
+    char *infoNamesArr[] = {"STATUS", "PID", "NAME", "TIME"};
+    for (int i = 0; i < NOTIF_ARR_SIZE; i++) // Print the info names.
+    {
+        mvwprintw(win, INIT_PRINT_Y - 1, i * (scrnWidth / NOTIF_ARR_SIZE) + 2, "%s\t", infoNamesArr[i]);
+    }
+
+    res = printNotifLines(win, lines, *offset, scrnWidth, scrnHeight); // Print the lines.
+    if (res)                                                           // Check if printing was successful.
+    {
+        wrefresh(win);
+        return 1;
+    }
+
+    freeHashSet(newProcsSet);
+    freeHashSet(oldProcsSet);
+    wrefresh(win);
+    return 0;
+}
+void notificationsWindow(void *data)
+{
+    thread_data_ptr procData = (thread_data_ptr)data;
+    pthread_cond_t *inputCond = procData->inputCond;
+    pthread_mutex_t *inputLock = procData->inputLock;
+
+    int *run = procData->run;
+    int *offset = procData->offset;
+    int *lineCount = procData->lineCount;
+    int scrnHeight;
+    int scrnWidth;
+    getmaxyx(stdscr, scrnHeight, scrnWidth);
+    int height = scrnHeight / 2 - scrnHeight / 5;
+
+    WINDOW *notifWindow = createSubWindow(mainWindow, height, scrnWidth, scrnHeight / 5 + INIT_PRINT_Y, 0);
+
+    if (notifWindow == NULL)
+    {
+        fprintf(stderr, "Failed to create proc window...");
+        exit(EXIT_FAILURE);
+    }
+    hash_set_ptr genSet = getNewHashSet();
+    notif_line_ptr lines = getNewNotifsLinePtr();
+    // proc window created
+    struct timespec remaining, request;
+    while (*run)
+    {
+        float interval_in_seconds = 60.0 / refreshRate;
+
+        // Separate into whole seconds and fractional seconds
+        request.tv_sec = (time_t)interval_in_seconds;
+        request.tv_nsec = (long)((interval_in_seconds - request.tv_sec) * 1e9);
+        if (waitOnInput)
+        {
+            pthread_mutex_lock(inputLock);           // Acquire lock.
+            pthread_cond_wait(inputCond, inputLock); // Wait for input to be finished.
+            pthread_mutex_unlock(inputLock);         // Unacquire lock.
+        }
+        if (addLinesToNotifications(notifWindow, genSet, lines, height, offset, lineCount))
+        {
+            continue;
+        }
+
+        nanosleep(&request, &remaining);
+    }
+    delwin(procWindow);
+    pthread_exit(NULL);
+}
 int notificationsMenu(int *offset, int *lineCount)
 {
     char *choices[] = {"Enable", "Disable"};
@@ -233,15 +461,18 @@ int sortMenu(int *offset, int *lineCount)
 
 int createGeneralMenu(char *choices[], int choicesSize, int *offset, int *lineCount)
 {
-    int scrnHeight;
-    int scrnWidth;
+    int scrnHeight, scrnWidth;
     getmaxyx(mainWindow, scrnHeight, scrnWidth);
     int height = scrnHeight / 5;
+
+    // create the window at the top left of the screen, with a height of 1/
     WINDOW *menuWin = createSubWindow(mainWindow, height, scrnWidth, 0, 0);
+    // make it visible
     wrefresh(menuWin);
+    // enable input
     keypad(menuWin, true);
-    int choice;
-    int highlight = 0;
+
+    int choice, highlight = 0;
     while (1)
     {
         for (int i = 0; i < choicesSize; i++)
@@ -264,13 +495,13 @@ int createGeneralMenu(char *choices[], int choicesSize, int *offset, int *lineCo
             break;
 
         case KEY_UP:
-            if (*offset > 0)
-                (*offset)--;
+            if (offset[0] > 0)
+                (offset[0])--;
             break;
 
         case KEY_DOWN:
-            if (*offset < *lineCount - height)
-                (*offset)++;
+            if (offset[0] < lineCount[0] - height)
+                (offset[0])++;
             break;
 
         case KEY_RESIZE:
@@ -333,6 +564,7 @@ int printLines(ps_line_ptr line, WINDOW *win, int offset, int scrnWidth, int scr
         {
             attron(COLOR_PAIR(1));
             mvwprintw(win, y, i * (scrnWidth / LINE_ARR_SIZE) + 2, "%s", line->info_arr[i]);
+            wrefresh(win);
             attroff(COLOR_PAIR(1));
         }
         y++;
@@ -343,7 +575,7 @@ int printLines(ps_line_ptr line, WINDOW *win, int offset, int scrnWidth, int scr
     return 0;
 }
 
-int freeAllLines(ps_line_ptr line)
+int freeAllPsLines(ps_line_ptr line)
 {
     while (line)
     {
@@ -383,6 +615,8 @@ ps_line_ptr getNewPsLinePtr()
 
 int getLinesForProc(ps_line_ptr line, int *lineCount)
 {
+    hash_set_ptr genSet = getNewHashSet();
+    hash_set_ptr newSet = getNewHashSet();
     if (waitOnInput)
         return 1;
 
@@ -439,6 +673,7 @@ int getLinesForProc(ps_line_ptr line, int *lineCount)
             i++;
         }
     }
+
     pclose(fp);
     return 0;
 }
@@ -457,7 +692,7 @@ int addLinesToProc(WINDOW *pw, int height, int *offset, int *lineCount)
     if (res)                                    // check for errors in getting the lines.
     {
         wrefresh(pw);
-        freeAllLines(lines); // Free all the allocated lines.
+        freeAllPsLines(lines); // Free all the allocated lines.
         return 1;
     }
 
@@ -483,11 +718,11 @@ int addLinesToProc(WINDOW *pw, int height, int *offset, int *lineCount)
     if (res)                                                    // Check if printing was successful.
     {
         wrefresh(pw);
-        freeAllLines(lines); // Free all the allocated lines.
+        freeAllPsLines(lines); // Free all the allocated lines.
         return 1;
     }
 
-    freeAllLines(lines); // Free all the allocated lines.
+    freeAllPsLines(lines); // Free all the allocated lines.
     wrefresh(pw);
     return 0;
 }
@@ -501,8 +736,7 @@ void createProcWindow(void *data)
     int *run = procData->run;
     int *offset = procData->offset;
     int *lineCount = procData->lineCount;
-    int scrnHeight;
-    int scrnWidth;
+    int scrnHeight, scrnWidth;
     getmaxyx(stdscr, scrnHeight, scrnWidth);
     int height = scrnHeight / 2;
     procWindow = createSubWindow(mainWindow, height, scrnWidth, scrnHeight / 2 - 2, 0);
@@ -516,13 +750,14 @@ void createProcWindow(void *data)
     // proc window created
 
     struct timespec remaining, request;
-    float interval_in_seconds = 60.0 / refreshRate;
 
-    // Separate into whole seconds and fractional seconds
-    request.tv_sec = (time_t)interval_in_seconds;
-    request.tv_nsec = (long)((interval_in_seconds - request.tv_sec) * 1e9);
     while (*run)
     {
+        float interval_in_seconds = 60.0 / refreshRate;
+
+        // Separate into whole seconds and fractional seconds
+        request.tv_sec = (time_t)interval_in_seconds;
+        request.tv_nsec = (long)((interval_in_seconds - request.tv_sec) * 1e9);
         if (waitOnInput)
         {
             pthread_mutex_lock(inputLock);           // Acquire lock.
@@ -560,30 +795,32 @@ int main(int argc, char *argv[])
     int index = 0;
     int res = 0;
     int run = 1;
-    int offset = 0;
-    int lineCount = 0;
-
+    int countLines[2] = {0}; // array used to keep track of how many lines there are in each screen. 0 - procWindow, 1 - notifWindow.
+    int offsets[2] = {0};    //  array used to keep track of the linees offset in each screen. 0 - procWindow, 1 - notifWindow.
     char *initialMenuChoices[] = {"RefreshRate", "Sort", "Filter", "Notifications", "Exit"};
     int (*menuFunctionsArr[])(int *, int *) = {refreshRateMenu,
                                                sortMenu,
                                                filterMenu,
+                                               notificationsMenu,
                                                exitFunction};
 
     pthread_t procsThread;
     pthread_cond_t inputCond = PTHREAD_COND_INITIALIZER;
     pthread_mutex_t inputLock = PTHREAD_MUTEX_INITIALIZER;
-    thread_data data = {&inputCond, &inputLock, &run, &offset, &lineCount};
+    thread_data data = {&inputCond, &inputLock, &run, &offsets[0], &countLines[0]};
     pthread_create(&procsThread, NULL, (void *)createProcWindow, (void *)&data);
 
     while (run)
     {
-        index = createGeneralMenu(initialMenuChoices, INIT_CHOICES_SIZE, &offset, &lineCount);
+        index = createGeneralMenu(initialMenuChoices, INIT_CHOICES_SIZE, offsets, countLines);
         if (index == -1 || index == INIT_CHOICES_SIZE - 1) // If an error occured or the user pressed Exit, close the program.
+        {
             run = 0;
+        }
 
         waitOnInput = 1; // tell procwindow to wait.
 
-        res = menuFunctionsArr[index](&offset, &lineCount); // Commit the user's requested action.
+        res = menuFunctionsArr[index](offsets, countLines); // Commit the user's requested action.
 
         pthread_mutex_lock(&inputLock);   // acquire lock.
         pthread_cond_signal(&inputCond);  // signal procwindow to continue.
